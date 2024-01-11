c211df97afddd2b0b6add71f62f85eab8c6f8ee8
Zongotripledozo <113369286+Zongotripledozo@users.noreply.github.com>
2024-01-11 16:38:57 +0100

    tg

--- PHPSkodajFromj/Views/accueilView.php
+++ PHPSkodajFromj/Views/accueilView.php
@@ -3,4 +3,4 @@
             <source src="./Assets/bgVidSkodaOctavia.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>
-
+        
---
+++ PHPSkodajFromj/Views/configView.php
@@ -0,0 +1,10 @@
+
+<img src=".\Assets\skoda\skoda-scala\S4-s0-essai-video-skoda-scala-587943-142941.jpg">
+
+<form action="index.php?page=config">
+    <label for="couleur">couleur</label>
+    <select name="couleur" id="couleur">
+        <option value="Bleu" name="bleu">Bleu</option>
+        <option value="Rouge" name="Rouge" >Rouge</option>
+        <option value="Noir" name="Noir" >Noir</option>
+        

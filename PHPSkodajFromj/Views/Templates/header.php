1679f6735d57b6290c985fe467c326f2838dbbbd
Beuhnnyto <adam.malek@efrei.net>
2024-01-11 16:40:56 +0100

    nav grosse barre tah les oufs

--- PHPSkodajFromj/Views/Templates/header.php
+++ PHPSkodajFromj/Views/Templates/header.php
@@ -6,5 +6,27 @@
    <title>Skoda 2.0</title>
    <meta name="description" content="DESCRIPTION">
    <link rel="stylesheet" href="./Views/Css/style.css">
+   <script src="./Views/Js/script.js" defer></script>
 </head>
 <body>
+
+   <header>
+      <nav>
+         <span><img src="./Assets/logo-skoda.png" alt="logo" height="25px"></span>
+         <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li>
+               <div class="dropdown-menu">
+                  <button class="dropdown-btn">Nos Catalogues &#11167;</button>
+                  <div class="dropdown-content">
+                     <a href="index.php?page?=catalogueVoiture ">Nos Voitures</a>
+                     <a href="index.php?page?=cataloguePieces ">Nos Pièces</a>
+                     
+                  </div>
+               </div>
+            </li>
+            <li><a href="">Nous contacter</a></li>
+            <li><a href="">Skoda &amp; Vous</a></li>
+         </ul>
+      </nav>
+   </header>

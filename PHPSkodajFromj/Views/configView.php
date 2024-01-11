1f29fdc750c138c39144daefb6aefa87833357d6
Zongotripledozo <113369286+Zongotripledozo@users.noreply.github.com>
2024-01-11 17:11:59 +0100

    giudsfguhjdsfjbhkdsqf

--- PHPSkodajFromj/Views/configView.php
+++ PHPSkodajFromj/Views/configView.php
@@ -1,10 +1,41 @@
 
 <img src=".\Assets\skoda\skoda-scala\S4-s0-essai-video-skoda-scala-587943-142941.jpg">
 
-<form action="index.php?page=config">
-    <label for="couleur">couleur</label>
+<form action="index.php?page=config" method="post">
+    <label for="couleur">Couleur</label>
     <select name="couleur" id="couleur">
-        <option value="Bleu" name="bleu">Bleu</option>
-        <option value="Rouge" name="Rouge" >Rouge</option>
-        <option value="Noir" name="Noir" >Noir</option>
-        
+      <option value="Bleu" name="bleu">Bleu</option>
+      <option value="Rouge" name="Rouge" >Rouge</option>
+      <option value="Noir" name="Noir" >Noir</option>
+      <option value="Blanc" name="Blanc">Blanc</option>
+    </select>
+    <br>
+    <h1>option</h1>
+    <label for="navigation">Navigation System</label>
+    <input type="checkbox" id="navigation" name="navigation" />
+    <br>
+    <label for="heated_seats">Heated Seats</label>
+    <input type="checkbox" id="heated_seats" name="heated_seats" />
+    <br>
+    <label for="air_conditioning">Air Conditioning</label>
+    <input type="checkbox" id="air_conditioning" name="air_conditioning" />
+    <br>
+    <label for="bluetooth_audio">Bluetooth Audio System</label>
+    <input type="checkbox" id="bluetooth_audio" name="bluetooth_audio" />
+    <br>
+    <label for="cruise_control">Cruise Control</label>
+    <input type="checkbox" id="cruise_control" name="cruise_control" />
+    <br>
+    <label for="sunroof">Sunroof</label>
+    <input type="checkbox" id="sunroof" name="sunroof" />
+    <br>
+    <label for="moteur">Motuer</label>
+    <select name="Moteur" id="Moteur">
+      <option value="V8" name="V8">V8</option>
+      <option value="V10" name="V10" >V10</option>
+      <option value="WR16" name="WR16" >WR16</option>
+      <option value="V18" name="V18">V18</option>
+      <option value="i6" name="i6">i6</option>
+    </select>
+    <button type="submit">Submit</button>
+  </form>
